b37f9482e24eac56faec8cccf8ef7b7f94c799f7
tac <tacccccc21@gmail.com>
2023-09-25 17:20:03 +0900

    noImplicitAny3

--- pages/posts/page/[page].tsx
+++ pages/posts/page/[page].tsx
@@ -11,7 +11,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   const numberOfPage = await getNumberOfPages();
 
-  const allTags = await getAllTags();
+  const allTags:any = await getAllTags();
 
   let params:any = [];
   for(let i = 1; i <= numberOfPage; i++){
