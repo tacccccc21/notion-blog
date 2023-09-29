b54e3a0ce14ffcc6bca4f0815e7b74c91b84fb2c
tac <tacccccc21@gmail.com>
2023-09-29 20:30:59 +0900

    fallbackFalse

--- pages/posts/page/[page].tsx
+++ pages/posts/page/[page].tsx
@@ -54,7 +54,7 @@ const BlogPageList = ({ postsByPage, numberOfPage ,allTags}: any) => {
 
 
     <main className='container w-full mt-16'>
-      <h1 className='text-5xl font-medium text-center mb-16'>Notion Blog🚀</h1>
+      <h1 className='text-5xl font-medium text-center mb-16'>Notion Next Blog</h1>
       <section className='sm:grid grid-cols-2 w-5/6 gap-3 mx-auto'>
       {postsByPage.map((post: any) =>
         <div className='' key={post.id}>
